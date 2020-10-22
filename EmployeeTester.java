c2d9abd2cb0e8c561c3de9bcf4a8194d6f2f2fdf
Vedhak6 <Vedhak6>
2020-10-22 21:22:50 +0530

    22 oct assignment

---
+++ EmployeeTester.java
@@ -0,0 +1,12 @@
+package com.xworkz.constructorChaining;
+
+public class EmployeeTester {
+
+	public static void main(String[] args) {
+		
+		 Employee obj = new Employee();
+	        obj.disp();
+
+	}
+
+}
